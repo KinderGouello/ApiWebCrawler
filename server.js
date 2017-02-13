86fe755827586402119bc34f8ec7a292e6af88f8
Rémy Gouello <rigouello@gmail.com>
2017-02-13 22:38:30 +0100

    fix(heroku): fix heroku config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const port = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
 const router = require('./routes/index.js')(app)
 
-router.listen(3000)
+router.listen(port)
