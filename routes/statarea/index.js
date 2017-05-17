c93f72c1080fcf352c312f3c467e200cbdf9daf2
Rémy Gouello <rigouello@gmail.com>
2017-05-17 21:58:41 +0200

    feat(statarea): add statarea users

--- routes/statarea/index.js
+++ routes/statarea/index.js
@@ -28,6 +28,35 @@ const route = (app, request, cheerio) => {
     });
   });
 
+  app.get('/statarea-users', (req, res, next) => {
+    request.get('http://www.statarea.com/predictions', (err, response, body) => {
+      if (err) {
+        next(Object.assign(err, { status: 502 }));
+      }
+
+      const $ = cheerio.load(body, { decodeEntities: false });
+      const games = $('.predictions').find('.match');
+      const results = [];
+
+      games.each((index, element) => {
+        const equipe_domicile = $(element).find('.hostteam').find('.name').text().trim();
+        const bets = $(element).find('.inforow').find('.userrow');
+
+        if (equipe_domicile) {
+          results.push({
+            equipe_domicile,
+            equipe_exterieur: $(element).find('.guestteam').find('.name').text().trim(),
+            domicile: bets.find('.vote1').find('.value').text().trim(),
+            nul: bets.find('.voteX').find('.value').text().trim(),
+            exterieur: bets.find('.vote2').find('.value').text().trim(),
+          });
+        }
+      });
+
+      res.send({ results });
+    });
+  });
+
   return app;
 };
 
