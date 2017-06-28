5f33e63dddfe04eb689fe1e04283d8efc4d91912
Rémy Gouello <rigouello@gmail.com>
2017-06-28 23:05:18 +0200

    feat(sentry): add sentry to heroku

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const express = require('express');
 
 const app = express();
+const Raven = require('raven');
+
+Raven.config(process.env.SENTRY_DNS).install();
+
+app.use(Raven.requestHandler());
+app.get('/', function mainHandler(req, res) {
+    throw new Error('Broke!');
+});
+app.use(Raven.errorHandler());
+
 const router = require('./routes/index.js')(app);
 
 router.listen(port);
